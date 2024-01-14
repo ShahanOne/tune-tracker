1b621f75f81b6e0ce613ba1f1aa3865ecd273fe8
Syed Shahan Hussain <shahanhussain9105@gmail.com>
2024-01-15 01:52:23 +0530

    more  style additions

--- components/ViewArtist.jsx
+++ components/ViewArtist.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const ViewArtist = ({name,songs}) => {
   return (
     <div className='bg-white rounded p-8 grid grid-cols-1 md:grid-cols-2 gap-8'>
-        <img src='/music.jpg' alt='poster' className='w-[26rem]'/>
+        <img src={name==="Mr Chedda" ?'/chedda.jpeg':'/music.jpg'} alt='poster' className='w-[26rem]'/>
         <div className=''>
 <p className='text-2xl text-black mb-2'>{name}</p>
 <p className='text-lg text-black'>Songs:</p>
